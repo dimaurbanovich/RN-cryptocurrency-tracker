ccf0d36d5b47c376fed95f251430af99280edae6
Dmitri <dmitri.urbanovich@gmail.com>
2022-02-08 19:20:17 +0300

    added coins and charts

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useMemo, useState } from "react";
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import ListItem from "./components/ListItem";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+import { SAMPLE_DATA } from "./assets/data/sampleData";
+import Chart from "./components/Chart";
+
+const ListHeader = () => (
+  <>
+    <View style={styles.titleWrapper}>
+      <Text style={styles.largeTitle}>Markets</Text>
+    </View>
+    <View style={styles.divider} />
+  </>
+);
 
 export default function App() {
+  const [selectedCoinData, setSelectedCoinData] = useState(null);
+
+  const bottomSheetModalRef = useRef(null);
+
+  const snapPoints = useMemo(() => ["45%"], []);
+
+  const openModal = (item) => {
+    setSelectedCoinData(item);
+    bottomSheetModalRef.current?.present();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomSheetModalProvider>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={SAMPLE_DATA}
+          renderItem={({ item }) => (
+            <ListItem
+              name={item.name}
+              symbol={item.symbol}
+              currentPrice={item.current_price}
+              priceChangePercentage7d={
+                item.price_change_percentage_7d_in_currency
+              }
+              logoUrl={item.image}
+              onPress={() => openModal(item)}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          ListHeaderComponent={<ListHeader />}
+        />
+      </SafeAreaView>
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+        style={styles.bottomSheet}
+      >
+        {selectedCoinData && (
+          <Chart
+            name={selectedCoinData.name}
+            symbol={selectedCoinData.symbol}
+            currentPrice={selectedCoinData.current_price}
+            priceChangePercentage7d={
+              selectedCoinData.price_change_percentage_7d_in_currency
+            }
+            logoUrl={selectedCoinData.image}
+            sparkline={selectedCoinData.sparkline_in_7d.price}
+          />
+        )}
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  titleWrapper: {
+    marginTop: 20,
+    paddingHorizontal: 16,
+  },
+  largeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginTop: 16,
+    marginHorizontal: 16,
+  },
+  bottomSheet: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: -4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
---
+++ components/Chart.js
@@ -0,0 +1,106 @@
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
+import {
+  ChartDot,
+  ChartPath,
+  ChartPathProvider,
+  ChartYLabel,
+} from "@rainbow-me/animated-charts";
+
+const Chart = ({
+  name,
+  symbol,
+  currentPrice,
+  priceChangePercentage7d,
+  logoUrl,
+  sparkline,
+}) => {
+  const priceChangeColor = priceChangePercentage7d > 0 ? "#34C759" : "#FF3B30";
+
+  const { width: SIZE } = Dimensions.get("window");
+
+  const formatUSD = (value) => {
+    "worklet";
+    if (value === "") {
+      return `$${currentPrice.toLocaleString("en-US", { currency: "USD" })}`;
+    }
+
+    const formatedValue = `$${parseFloat(value)
+      .toFixed(2)
+      .replace(/\d(?=(\d{3})+\.)/g, "$&,")}`;
+    return formatedValue;
+  };
+
+  return (
+    <ChartPathProvider
+      data={{ points: sparkline, smoothingStrategy: "bezier" }}
+    >
+      <View style={styles.chartWrapper}>
+        <View style={styles.titlesWrapper}>
+          <View style={styles.upperTitles}>
+            <View style={styles.upperLeftTitle}>
+              <Image style={styles.image} source={{ uri: logoUrl }} />
+              <Text style={styles.subtitle}>
+                {name} ({symbol.toUpperCase()})
+              </Text>
+            </View>
+            <Text style={styles.subtitle}>7d</Text>
+          </View>
+          <View style={styles.lowerTitles}>
+            <ChartYLabel format={formatUSD} style={styles.boldTitle} />
+            <Text style={[styles.title, { color: priceChangeColor }]}>
+              {priceChangePercentage7d.toFixed(2)}%
+            </Text>
+          </View>
+        </View>
+        <View style={styles.chartLineWrapper}>
+          <ChartPath height={SIZE / 2} stroke="black" width={SIZE} />
+          <ChartDot style={{ backgroundColor: "black" }} />
+        </View>
+      </View>
+    </ChartPathProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  chartWrapper: {
+    marginVertical: 16,
+  },
+  titlesWrapper: {
+    marginHorizontal: 16,
+  },
+  upperTitles: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  upperLeftTitle: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    width: 24,
+    height: 24,
+    marginRight: 4,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: "#A9ABB1",
+  },
+  lowerTitles: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  boldTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 18,
+  },
+  chartLineWrapper: {
+    marginTop: 40,
+  },
+});
+
+export default Chart;
